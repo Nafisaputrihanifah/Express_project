1e4e4e92fc6d71facacde21ba1d1baf6c6eb9df9
Nazriilhamsyah <nazril_062@smkassalaambandung.sch.id>
2024-02-01 02:50:12 -0500

    postman edisi 2

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const authController = require('./authController');
+const authController = require('./userontroller');
 
 module.exports = {
     auth: authController,
+    user: userController,
 };
